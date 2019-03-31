00c5277b4af1e595c843bf878907d0275ea8f68f
qiaomu <qiaomu@item.com>
2019-03-31 10:03:16 +0800

    添加TemplateController --XX组员

---
+++ pinyougou-web-user/src/main/java/cn/itcast/core/controller/TemplateController.java
@@ -0,0 +1,9 @@
+package cn.itcast.core.controller;
+
+public class TemplateController {
+    public static void main(String[] args) {
+        System.out.println(123456);
+        System.out.println(123456);
+        System.out.println(123456);
+    }
+}
